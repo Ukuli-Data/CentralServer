340e419cd7bee40b1da57a9120d8fb0453ac36db
Tomi Toivio <tomi.toivio@ukuli.fi>
2021-02-16 12:38:03 +0200

    Flask

--- app.py
+++ app.py
@@ -5,5 +5,8 @@ app = Flask(__name__)
 def hello_world(): 
     return 'Hello to the World of Flask!' 
  
+app = Flask(__name__, static_folder='./static/') 
+# app.config.from_object('configuration.DevelopmentConfig') 
+
 if __name__ == '__main__': 
     app.run() 
